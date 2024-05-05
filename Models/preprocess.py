ffadc59b1f4b502d725ccdd21d7d3da6bfb6d993
VoidKeishi <phananha4@gmail.com>
2024-05-05 23:51:51 +0700

    Restructure and rename columns

--- Models/preprocess.py
+++ Models/preprocess.py
@@ -3,21 +3,11 @@
 import pandas as pd
 import string
 import emoji_vietnamese
-# For vihsd dataset
 def preprocess_data(data):
-    data['free_text'] = data['free_text'].str.replace(
+    data['text'] = data['text'].str.replace(
         r'http\S+', '', regex=True).replace(r'www\S+', '', regex=True)
-    data['free_text'] = data['free_text'].str.replace(
+    data['text'] = data['text'].str.replace(
         '['+string.punctuation+']', '', regex=True)
-    data['free_text'] = data['free_text'].str.lower()
-    data['free_text'] = data['free_text'].apply(emoji_vietnamese.demojize)
-    return data
-# For victsd dataset
-def preprocess_data(data):
-    data['Comment'] = data['Comment'].str.replace(
-        r'http\S+', '', regex=True).replace(r'www\S+', '', regex=True)
-    data['Comment'] = data['Comment'].str.replace(
-        '['+string.punctuation+']', '', regex=True)
-    data['Comment'] = data['Comment'].str.lower()
-    data['Comment'] = data['Comment'].apply(emoji_vietnamese.demojize)
+    data['text'] = data['text'].str.lower()
+    data['text'] = data['text'].apply(emoji_vietnamese.demojize)
     return data
