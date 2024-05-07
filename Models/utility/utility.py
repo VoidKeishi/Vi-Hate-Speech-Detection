96ca77878225427e6324e35ce90f7b42f836c85e
VoidKeishi <phananha4@gmail.com>
2024-05-07 21:23:16 +0700

    Change module to utility, add load_data

--- Models/utility/utility.py
+++ Models/utility/utility.py
@@ -1,5 +1,22 @@
+import pandas as pd  # type: ignore
 import string
 import emoji_vietnamese  # type: ignore
+
+def load_data(dataset='vihsd'):
+    if dataset == 'vihsd':
+        train = pd.read_csv('../Dataset_Reformat/train_vihsd.csv')
+        dev = pd.read_csv('../Dataset_Reformat/dev_vihsd.csv')
+        test = pd.read_csv('../Dataset_Reformat/test_vihsd.csv')
+    elif dataset == 'victsd':
+        train = pd.read_csv('../Dataset_Reformat/train_victsd.csv')
+        dev = pd.read_csv('../Dataset_Reformat/dev_victsd.csv')
+        test = pd.read_csv('../Dataset_Reformat/test_victsd.csv')
+    elif dataset == 'merged':
+        train = pd.read_csv('../Dataset_Reformat/train_merged.csv')
+        dev = pd.read_csv('../Dataset_Reformat/dev_merged.csv')
+        test = pd.read_csv('../Dataset_Reformat/test_merged.csv')
+    return train, dev, test
+
 
 def preprocess_data(
     data,
@@ -8,11 +25,10 @@ def preprocess_data(
     lowercase=True,
     stopword=False,
     emoji=False
-    ):
+):
     # Load stopwords
-    with open('./preprocess/Stopwords/vietnamese-stopwords.txt', 'r') as f:
+    with open('./preprocess/Stopwords/vietnamese-stopwords.txt', 'r', encoding='utf-8') as f:
         stopwords = f.read().splitlines()
-
     # Function to remove stopwords
     def remove_stopwords(text):
         words = text.split()
